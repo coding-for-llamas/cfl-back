e840391cd621242fd9197c8fb2b79333a5b93553
Rhayven <codingforllamas@gmail.com>
2020-03-07 22:39:19 -0500

    Changes permissions and updates depencies

--- postinstallcaringChildDaycare.sh
+++ postinstallcaringChildDaycare.sh
@@ -9,12 +9,12 @@ then
     BRANCH=dev
 fi
 
-if [ ! -d cfl-front ]; then
-    (git clone https://github.com/coding-for-llamas/cfl-front)
+if [ ! -d caring-child-daycare ]; then
+    (git clone https://github.com/coding-for-llamas/caring-child-daycare)
 fi
 
 (
-cd cfl-front || exit;
+cd caring-child-daycare || exit;
 git stash;
 git checkout $BRANCH;
 git pull;
@@ -23,11 +23,11 @@ cd ..;
 
 if [ -f .env ];
 then
-  (cp .env cfl-front/;
+  (cp .env caring-child-daycare/;
   )
 fi
 
 (
-cd cfl-front;
+cd caring-child-daycare;
 yarn install;
 )
