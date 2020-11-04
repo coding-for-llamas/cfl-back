68ec5c8fe550899f9ce119e3da7a5d27fbe895f9
unesdala <codingforllamas@gmail.com>
2020-11-04 07:42:01 -0500

    circleCI fixes

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import inquiry from './model/inquiry';
 
 const router = express.Router();
 
-export default function route(app) {
+export default function route(app: any) {
   app.use(router);
   router.use('/inquiry', inquiry);
 }
