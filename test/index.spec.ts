3f3080938597d85001d5677773238f02f9bff126
Rhayven <codingforllamas@gmail.com>
2021-05-06 02:13:02 -0400

    Updates tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,27 @@
-import { Server } from 'http';
 import request from 'supertest';
 import app from '../src/index';
 
 const AllowUrl = JSON.parse(process.env.AllowUrl || '{}');
 
 describe('Index test', () => {
-  let allowedUrl: any, r: { status: any; }, server: Server, agent: request.SuperAgentTest;
-  beforeAll(() => new Promise((done) => {
+  let allowedUrl: any, r, server: any, agent: any;
+  // eslint-disable-next-line jest/no-done-callback
+  beforeAll((done) => {
     server = app.listen(7000, () => {
       agent = request.agent(server);
       return done();
     });
-  }));
-  beforeEach(() => new Promise((done) => {
+  });
+  // eslint-disable-next-line jest/no-done-callback
+  beforeEach((done) => {
     [allowedUrl] = AllowUrl.urls;
     done();
-  }));
-  afterAll(() => new Promise((done) => server && server.close(done)));
-  it('should return status 200 when use -> app.get', async () => {
+  });
+  // eslint-disable-next-line jest/no-done-callback
+  afterAll((done) => server && server.close(done));
+  it('should return status 200 when use -> app.get to unknown route', async () => {
     r = await agent
-      .get('/anyUrl')
+      .get('/unknown')
       .set({ origin: allowedUrl })
       .set('authorization', 'Bearer ');
     expect(r.status).toBe(200);
@@ -31,15 +33,21 @@ describe('Index test', () => {
       .set('authorization', 'Bearer ');
     expect(r.status).toBe(200);
   });
-  it('should return 500 error', async () => {
+  it('should return 404 error', async () => {
     r = await agent
       .delete('/bogus')
       .set({ origin: 'bogus' })
       .set('authorization', 'Bearer ');
-    expect(r.status).toBe(500);
+    expect(r.status).toBe(404);
   });
+  it('returns 500 error', async () => {
+    r = await agent
+      .get('/')
+      .set('undefined');
+    expect(r.status).toBe(500);
+  }); 
   it('should wait unit tests finish before exiting', async () => { // eslint-disable-line jest/expect-expect
-    const delay = (ms: number) => new Promise((resolve) => setTimeout(() => resolve(true), ms));
+    const delay = (ms: any) => new Promise((resolve) => setTimeout(() => resolve(true), ms));
     await delay(4000);
   });
 });
--- test/jest/inquiry/inquiry.spec.ts
+++ test/jest/inquiry/inquiry.spec.ts
@@ -3,7 +3,7 @@ import app from '../../../src/index';
 
 describe('Inquiry Router', () => {
   afterAll(async () => {
-    await new Promise((resolve) => setTimeout(() => resolve(), 500));
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 500));
   });
 
   // r = request
