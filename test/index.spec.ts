7b5372b44da7ea4f5004c7f03abdda29ad187b04
Rhayven <codingforllamas@gmail.com>
2021-05-06 02:34:47 -0400

    updates testing

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,8 @@
 import request from 'supertest';
 import app from '../src/index';
 
-const AllowUrl = JSON.parse(process.env.AllowUrl || '{}');
-
 describe('Index test', () => {
-  let allowedUrl: any, r, server: any, agent: any;
+  let r: { status: any; }, server: any, agent: any;
   // eslint-disable-next-line jest/no-done-callback
   beforeAll((done) => {
     server = app.listen(7000, () => {
@@ -13,41 +11,18 @@ describe('Index test', () => {
     });
   });
   // eslint-disable-next-line jest/no-done-callback
-  beforeEach((done) => {
-    [allowedUrl] = AllowUrl.urls;
-    done();
-  });
+
   // eslint-disable-next-line jest/no-done-callback
   afterAll((done) => server && server.close(done));
-  it('should return status 200 when use -> app.get to unknown route', async () => {
-    r = await agent
-      .get('/unknown')
-      .set({ origin: allowedUrl })
-      .set('authorization', 'Bearer ');
-    expect(r.status).toBe(200);
-  });
-  it('should return status 200 when use -> app.get at root', async () => {
-    r = await agent
-      .get('/')
-      .set({ origin: allowedUrl })
-      .set('authorization', 'Bearer ');
-    expect(r.status).toBe(200);
-  });
-  it('should return 404 error', async () => {
+  it('should return 500 error', async () => {
     r = await agent
       .delete('/bogus')
       .set({ origin: 'bogus' })
       .set('authorization', 'Bearer ');
-    expect(r.status).toBe(404);
-  });
-  it('returns 500 error', async () => {
-    r = await agent
-      .get('/')
-      .set('undefined');
     expect(r.status).toBe(500);
-  }); 
+  });
   it('should wait unit tests finish before exiting', async () => { // eslint-disable-line jest/expect-expect
-    const delay = (ms: any) => new Promise((resolve) => setTimeout(() => resolve(true), ms));
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(() => resolve(true), ms));
     await delay(4000);
   });
 });
